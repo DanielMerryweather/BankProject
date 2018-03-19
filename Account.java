3dceccc4ed2a60836f3f7e6dc37519c57795bf6c
PunitThanki <37454295+PunitThanki@users.noreply.github.com>
2018-03-19 13:32:43 -0600

    Add files via upload

--- Account.java
+++ Account.java
@@ -2,13 +2,18 @@ package defaultpackage;
 
 import java.util.Scanner;
 
+/**
+ * @author Punit Thanki
+ *Start Date:
+ *End Date: 
+ *
+ */
 public class Account {
-	String id;
+	public String id;
 	public double balance;
 	Scanner input = new Scanner(System.in);
-	public Account(double balance, String i){
-		String id;
-		this.id = 0;
+	public Account(){
+		this.id = "";
 		this.balance = 0;
 	}
 	public Account(double balance, String id){
@@ -18,7 +23,15 @@ public class Account {
 	public void deposit(){
 		System.out.println("How much would you like to deposit?");
 		double amountD = input.nextDouble();
-		amountD = balance + amountD;
+		while(amountD<0){
+			System.out.println("Invalid input!! Please please enter a number greater than 0");
+			this.balance+=amountD;
+		}
+		while(!input.hasNextInt()){
+			System.out.println("That is not a valid input!! Please enter a number");
+			input.next();
+		}
+		this.balance+=amountD;				
 		
 	}
 	public void withdraw(){
@@ -27,15 +40,19 @@ public class Account {
 		while(balance<amountW){
 			System.out.println("Insufficient Funds! You only have" + balance + "dollars");
 		}
+		while(amountW<0){
+			System.out.println("Invalid input!! Please please enter a number greater than 0");
+			this.balance-=amountW;
+		}
 		if(balance>amountW){
-			amountW = balance - amountW;
+			this.balance-=amountW;
 		}
 	}
 	
-	public int getId() {
+	public String getId() {
 		return id;
 	}
-	public void setId(int id) {
+	public void setId(String id) {
 		this.id = id;
 	}
 	public double getBalance() {
@@ -44,11 +61,8 @@ public class Account {
 	public void setBalance(double balance) {
 		this.balance = balance;
 	}
-	public static void main(String[] args){
-		
-
-	}
-
+	
 }
+
 
 
