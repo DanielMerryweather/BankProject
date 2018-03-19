f56683d55f139b154509beecd166a49f091cc235
PunitThanki <37454295+PunitThanki@users.noreply.github.com>
2018-03-19 12:51:11 -0600

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,53 @@
+package defaultpackage;
+
+import java.util.Scanner;
+
+public class Account {
+	public int id;
+	public double balance;
+	Scanner input = new Scanner(System.in);
+	public Account(){
+		this.id = 0;
+		this.balance = 0;
+	}
+	public Account(double balance, int id){
+		this.id = id;
+		this.balance = balance;
+	}
+	public void deposit(){
+		System.out.println("How much would you like to deposit?");
+		double amountD = input.nextDouble();
+		amountD = balance + amountD;
+		
+	}
+	public void withdraw(){
+		System.out.println("How much would you like to withdraw?");
+		double amountW = input.nextDouble();
+		while(balance<amountW){
+			System.out.println("Insufficient Funds! You only have" + balance + "dollars");
+		}
+		if(balance>amountW){
+			amountW = balance - amountW;
+		}
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	public static void main(String[] args){
+		
+
+	}
+
+}
+
+
