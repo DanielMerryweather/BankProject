38fb86723ae7605c3bea8174adf34b9a8dc28462
PunitThanki <37454295+PunitThanki@users.noreply.github.com>
2018-03-20 14:34:56 -0600

    Add files via upload

--- Account.java
+++ Account.java
@@ -1,13 +1,14 @@
 package defaultpackage;
-
 import java.util.Scanner;
 
 /**
  * @author Punit Thanki
- *Start Date:
- *End Date: 
- *
+ * Start Date: 2018/03/14
+ * End Date: 20180/03/19 
+ * UserID: 20218002	
+ * Version 1
  */
+
 public class Account {
 	public String id;
 	public double balance;
@@ -23,15 +24,17 @@ public class Account {
 	public void deposit(){
 		System.out.println("How much would you like to deposit?");
 		double amountD = input.nextDouble();
+		this.balance+=amountD;	
 		while(amountD<0){
 			System.out.println("Invalid input!! Please please enter a number greater than 0");
 			this.balance+=amountD;
 		}
-		while(!input.hasNextInt()){
+		while(!input.hasNextDouble()){
 			System.out.println("That is not a valid input!! Please enter a number");
 			input.next();
 		}
-		this.balance+=amountD;				
+		
+		System.out.println("Your new balance is " + this.balance);
 		
 	}
 	public void withdraw(){
@@ -39,14 +42,20 @@ public class Account {
 		double amountW = input.nextDouble();
 		while(balance<amountW){
 			System.out.println("Insufficient Funds! You only have" + balance + "dollars");
+			
 		}
 		while(amountW<0){
 			System.out.println("Invalid input!! Please please enter a number greater than 0");
 			this.balance-=amountW;
 		}
+		while(!input.hasNextDouble()){
+			System.out.println("That is not a valid input!! Please enter a number");
+			input.next();
+		}
 		if(balance>amountW){
 			this.balance-=amountW;
 		}
+		System.out.println("Your new balance is " + this.balance);
 	}
 	
 	public String getId() {
